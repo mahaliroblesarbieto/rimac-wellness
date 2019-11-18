2a235ae930d660c3d2b420b32cae0bd9722b3bc3
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-11-17 23:04:45 -0500

    youtubecontainer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 import './App.scss';
-import img from './assets/Ilustración.png'
+import img from './assets/Ilustración.png';
+import YouTubeContainer from './YoutubeContainer';
 
 export default class TransmisionWellness extends Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      name: '',
-      email: '',
-      cellphone: '',
-      nameActive: 'active',
-      emailActive: 'active',
-      cellphoneActive: 'active',
-      validName: true,
-      validEmail: true,
-      validCellphone: true,
-      disabledButton: false,
-      blocking: false,
+      videoId:'2g811Eo7K8U'
     }
   }
   
@@ -42,14 +33,20 @@ export default class TransmisionWellness extends Component {
             </div>
           <div className="second-section">
             <div className=" container-inscripcion">
-              <div className="text-container ">
-                <div className="video">
+            <div className="text-container ">
+            {this.state.videoId !== '' ? 
+            <YouTubeContainer/> : 
+            <>
+              <div className="video">
                 <div className="text-video">
-                <p className="letter-video">En pocos minutos comenzará <b>la transmisión en vivo</b></p>
+                  <p className="letter-video">En pocos minutos comenzará <b>la transmisión en vivo</b></p>
                 </div>
-                </div>
-                <img src={img} alt="img-majo" className="img-majo"/>
               </div>
+              <img src={img} alt="img-majo" className="img-majo"/>
+              </>
+            }
+            </div> 
+
               <div className="form-container">
                 <div className="comments">
                   <div className="text subtitle-text">
---
+++ src/YoutubeContainer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+import './App.scss';
+ 
+class YouTubeContainer extends React.Component {
+  render() {
+    const opts = {
+    //   height: '390',
+    //   width: '440',
+    //   width: 370,
+    //   height: 600,
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+ 
+    return (
+      <YouTube
+        videoId="2g811Eo7K8U"
+        opts={opts}
+        className={"video-width"} 
+        onReady={this._onReady}
+      />
+    );
+  }
+ 
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+}
+
+export default YouTubeContainer;
